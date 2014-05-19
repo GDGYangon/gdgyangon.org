eb6842d6b008f01c89d9dd28186d83313bc0cf47
Tin Aung Lin <tinaunglinn.eng91@gmail.com>
2014-05-20 02:25:10 +0630

    Skel Grid Config

--- app.go
+++ app.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 	"net/http"
 )
 
 func init() {
 	m := martini.Classic()
-	m.Get("/events", func() string {
-		return "Events will be here"
+	m.Use(render.Renderer(render.Options{
+		Layout: "main",
+	}))
+
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "Hello World", "Hello World Content")
 	})
-	m.Get("/", func() string {
-		return "Hello world!"
+
+	m.Get("/events", func(r render.Render) {
+		r.HTML(200, "GDG( Yangon ) MeetUps", "Nothing yet")
 	})
+
 	m.Use(martini.Static("assets"))
 	http.Handle("/", m)
 }
---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+window._skel_config = {
+  prefix: "style",
+  resetCSS: true,
+  boxModel: "border",
+  grid: { gutters: 30 },
+  container: "fluid", 
+  breakpoints: {
+    wide: { range: "1200-", containers: "fluid", grid: { gutters: 50 } }, 
+    narrow: { range: "481-1199", containers: "fluid" },
+    mobile: { range: "-480", containers: "fluid", lockViewport: true, grid: { collapse: true } } 
+    }
+}; 
